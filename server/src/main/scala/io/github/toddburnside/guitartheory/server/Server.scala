552a9fe644c71430be109e4c7b345625bce65775
todd <canoebum@gmail.com>
2019-05-27 19:24:17 -0700

    Modify server startup

--- server/src/main/scala/io/github/toddburnside/guitartheory/server/Server.scala
+++ server/src/main/scala/io/github/toddburnside/guitartheory/server/Server.scala
@@ -2,22 +2,20 @@ package io.github.toddburnside.guitartheory.server
 
 import cats.effect._
 import cats.syntax.functor._
+import org.http4s.server.{Server => Http4sServer}
 import org.http4s.server.blaze.BlazeServerBuilder
 
 object Server extends IOApp {
-  override def run(args: List[String]): IO[ExitCode] =
-    new HttpServer[IO].server.as(ExitCode.Success)
-}
+  def createServer[F[_] : ContextShift: ConcurrentEffect: Timer] : Resource[F, Http4sServer[F]] = {
+    val module = new Module[F]
 
-class HttpServer[F[_]: ConcurrentEffect: ContextShift] {
-
-  private val module = new Module[F]
-
-  def server: F[Unit] =
     BlazeServerBuilder[F]
       .bindHttp(8080, "0.0.0.0")
       .withHttpApp(module.httpApp)
-      .serve
-      .compile
-      .drain
+      .resource
+  }
+
+  override def run(args: List[String]): IO[ExitCode] =
+    createServer.use(_ => IO.never).as(ExitCode.Success)
 }
+
